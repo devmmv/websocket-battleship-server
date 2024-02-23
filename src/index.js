ff1032623801d04715610edb0c4660f4948f6043
Mikhail <nodeprog@gmail.com>
2024-02-23 19:52:39 +0300

    init: start ws-task

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const index_1 = require("./http_server/index");
+const HTTP_PORT = 8181;
+console.log(`Start static http server on the ${HTTP_PORT} port!`);
+index_1.httpServer.listen(HTTP_PORT);
